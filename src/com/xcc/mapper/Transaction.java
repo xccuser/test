83f72944d87ad410b03d4dc8265511a7bb90eef0
xccuser <249790311@qq.com>
2017-12-26 16:58:09 +0800

    test

--- src/com/xcc/mapper/Transaction.java
+++ src/com/xcc/mapper/Transaction.java
@@ -3,10 +3,17 @@
  */
 package com.xcc.mapper;
 
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
 /**
  *  Administrator
  *
  */
 public interface Transaction {
-
+	Connection  getConnection() throws SQLException;  
+	void commit();
+	void rollback() throws SQLException;
+	void close() throws SQLException;
 }
